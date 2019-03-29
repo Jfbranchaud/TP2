1a397fd63440526e0a1a30f39461e35c03123411
jeffxsi <jfbranchaud40@gmail.com>
2019-03-28 21:36:14 -0400

    ajout animation logo

--- js/animation.js
+++ js/animation.js
@@ -1,9 +1,11 @@
 window.addEventListener('load',function(){
 
     console.log('passe')
+    let elmLogo = document.querySelector('.site-branding .site-logo')
     let elmTitre = document.querySelector('.site-branding .site-title')
     let elmDescription = document.querySelector('.site-branding .site-description')
 
+    elmLogo.classList.add('anim1')
     elmTitre.classList.add('anim1')
     elmDescription.classList.add('anim3')
 })
